11144e02114221bfa43061b2e22a1b11da8ea6de
Yash M <95099910+Yash-Marade@users.noreply.github.com>
2022-01-19 17:52:00 +0530

    Add files via upload

--- ID_Validate.java
+++ ID_Validate.java
@@ -2,6 +2,11 @@ import java.util.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+/* It have groups of three characters separated by "_".
+- Can only contain numbers 0-9 and characters a-z and A-Z.(other than "_")
+- Contains 12 characters excluding the "_" 
+    Sample id - ADI_239_Sep_B37 */
+
 public class ID_Validate {
 	public static void main(String[] args ) {
 		
@@ -9,12 +14,12 @@ public class ID_Validate {
 		System.out.println("Enter id");
 		String id = s.nextLine();
 		
-		// Regula Expression 
+// Regula Expression  
 		String idREgex = "[a-zA-Z0-9]{3}[_][a-zA-Z0-9]{3}[_][a-zA-Z0-9]{3}[_][a-zA-Z0-9]{3}";
 	    
-		//compile method from regex
+// compile method from regex
 		Pattern pt = Pattern.compile(idREgex);
-		//match method from regex
+// match method from regex
 		Matcher mt = pt.matcher(id);
 		
 		boolean result = mt.matches();
