9c38510b8c7d85a6253d55f213b26ba2d49007da
Yash M <95099910+Yash-Marade@users.noreply.github.com>
2022-03-05 11:43:48 +0530

    Add files via upload

---
+++ alarm.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+
+public class alarm {
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter day(from 0 to 6) :");
+		int day = sc.nextInt();
+		System.out.println("Enter true or false for vacation ");
+		boolean vacation = sc.nextBoolean();
+		if ( day == 0 || day == 1 || day == 2 || day == 3 || day == 4 || day == 5 || day == 6) {
+			if(vacation == false) {
+				System.out.println("7:00 AM");
+			} else {
+				System.out.println("10:00 AM");
+			}
+			
+		} else {
+			System.out.println("invalid Input");
+			
+		}
+		
+		
+		
+	}
+ 
+}
